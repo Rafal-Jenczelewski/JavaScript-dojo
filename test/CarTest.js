def68185c4f61929890bc268fc35cd5fa4008958
Rafal Jenczelewski <rafal.jenczelewski@nokia.com>
2018-07-25 12:28:25 +0200

    Changed tests

--- test/CarTest.js
+++ test/CarTest.js
@@ -1,26 +1,28 @@
 import {Car} from "../src/Car";
 
 describe('Car', () => {
-    let Car = undefined;
-
-    beforeEach(function () {
-        car = Car(100, 10);
-    });
-
     it('returns object with proper properties', () => {
-        expect(car).toEqual({getFuelLevel: function(){}, driveFor: function(){}})
+        let car = Car(100, 10);
+        expect(car).toHaveProperty('getFuelLevel');
+        expect(car).toHaveProperty('driveFor');
+
+        let keys = Object.keys(car);
+        expect(keys.length).toEqual(2);
     });
 
     it('returns 100% fuel at begging', () => {
+        let car = Car(100, 10);
         expect(car.getFuelLevel()).toEqual('100%');
     });
 
     it('has 10% less fuel after driving for 1 km', () => {
+        let car = Car(100, 10);
         car.driveFor(1);
         expect(car.getFuelLevel()).toEqual('90%');
     });
 
     it('has 0% fuel after driving for 11 km', () => {
+        let car = Car(100, 10);
         car.driveFor(11);
         expect(car.getFuelLevel()).toEqual("0%");
     })
