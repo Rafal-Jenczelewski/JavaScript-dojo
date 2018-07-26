96ccae96587859d007fbc891c8a20b92e438b9d7
Rafal Jenczelewski <rafal.jenczelewski@nokia.com>
2018-07-26 11:35:33 +0200

    Added tests and exercise

---
+++ src/BuildQueries.js
@@ -0,0 +1,16 @@
+/**
+ * This exercise is real-world example taken from CM Rule Editor rewrite.
+ * This function accepts array of rules. A rule is object like this:
+ *  {
+ *      name: string,
+ *      owner: string,
+ *      date: Date
+ *  }
+ *
+ *  You need to modify this function so it returns array of strings taken from name and owner of rule.
+ *  Every rule should generate two strings in returned array - one for name and one for owner.
+ */
+
+export const BuildQueries = function(rules) {
+
+};
---
+++ test/BuildQueries.test.js
@@ -0,0 +1,60 @@
+import {BuildQueries} from "../src/BuildQueries";
+
+describe('BuildQueries', () => {
+    it('should return empty array when given empty array', () => {
+        let queries = BuildQueries([]);
+
+        expect(queries.length).toBe(0);
+    });
+
+    it('should return 2 strings when given one rule', () => {
+        let queries = BuildQueries([{name: 'name1', owner: 'owner1', date: new Date()}]);
+
+        expect(queries.length).toBe(2);
+    });
+
+    it('should contain proper strings', () => {
+        let queries = BuildQueries([{name: 'name1', owner: 'owner1', date: new Date()}]);
+
+        expect(queries.includes('name1')).toBeTruthy();
+        expect(queries.includes('owner1')).toBeTruthy();
+    });
+
+    it('should return proper array', () => {
+        let rules = [
+            {
+                name: 'name1',
+                owner: 'owner1',
+                date: new Date()
+            },
+            {
+                name: 'name2',
+                owner: 'owner2',
+                date: new Date()
+            },
+            {
+                name: 'name3',
+                owner: 'owner4',
+                date: new Date()
+            },
+            {
+                name: 'name4',
+                owner: 'owner4',
+                date: new Date()
+            },
+            {
+                name: 'name5',
+                owner: 'owner5',
+                date: new Date()
+            }
+        ];
+
+        let queries = BuildQueries(rules);
+
+        expect(queries.length).toBe(rules * 2);
+        for (const rule of rules) {
+            expect(queries.includes(rule.name));
+            expect(queries.includes(rule.owner));
+        }
+    });
+});
