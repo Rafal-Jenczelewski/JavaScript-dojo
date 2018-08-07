4584d7d89c584b3e5918b01515aaff3cb4c0bc5d
Rafal Jenczelewski <rafal.jenczelewski@nokia.com>
2018-08-07 09:26:44 +0200

    First commit

---
+++ src/Cache.js
@@ -0,0 +1,12 @@
+/**
+ * In this exercise, you need to:
+ * Write a function that will return data about given user from almost the same mock as last time.
+ *
+ * This function should cache data that was already requested.
+ *
+ * Return value should be just user object taken from mock.
+ */
+
+export const Cache = function(name) {
+
+};
