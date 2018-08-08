5979dfb38bb8909dbcbc20d64b9319e642fc6526
Rafal Jenczelewski <rafal.jenczelewski@nokia.com>
2018-08-08 11:34:51 +0200

    Added tests

--- test/Insert.test.js
+++ test/Insert.test.js
@@ -1,5 +1,33 @@
 import {Insert} from "../src/Insert";
 
 describe('Insert', () => {
+    let texts = ["test1", "test2", "test3"];
 
+    it('should insert given text at the end with \'suf\' place', () => {
+        texts.forEach((text) => {
+            let output = Insert(text, "suf");
+
+            expect(output.indexOf(text)).toEqual(output.length - text.length);
+        })
+    });
+
+    it('should insert given text at the start with \'pre\' place', () => {
+        texts.forEach((text) => {
+            let output = Insert(text, "pre");
+
+            expect(output.indexOf(text)).toEqual(0);
+        })
+    });
+
+    it('should insert given text in the middle with \'middle\' and default place', () => {
+        texts.forEach((text) => {
+            let output = Insert(text, "middle");
+
+            expect(output.indexOf(text)).toBeGreaterThan(0);
+
+            output = Insert(text, "middle");
+
+            expect(output.indexOf(text)).toBeGreaterThan(0);
+        })
+    });
 });
